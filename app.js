61f397d03645416b55fb7f04b3e8ef403b07f2d5
Shinsaku Okazaki <golisaku4639@gmail.com>
2019-01-11 23:34:56 +0000

    add mlab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp");
+//mongoose.connect("mongodb://localhost:27017/yelp_camp");
+mongoose.connect("mongodb://shinsaku:shinsaku4639@ds027165.mlab.com:27165/yelpcampbyshin");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 app.use(express.static(__dirname +"/public"));
