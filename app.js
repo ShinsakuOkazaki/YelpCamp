8d102ec6b174d03c96057b3c213bad58faf918cc
Shinsaku Okazaki <golisaku4639@gmail.com>
2019-01-11 23:49:16 +0000

    updata dburl

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-//mongoose.connect("mongodb://localhost:27017/yelp_camp");
-mongoose.connect("mongodb://shinsaku:shinsaku4639@ds027165.mlab.com:27165/yelpcampbyshin");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://shinsaku:shinsaku4639@ds027165.mlab.com:27165/yelpcampbyshin");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 app.use(express.static(__dirname +"/public"));
